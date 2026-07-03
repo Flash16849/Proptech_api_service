d793b0db40fc27f6d64c479c194738692f7e948c
Flash16849 <namquan100803@gmail.com>
2026-07-03 16:43:11 +0700

    Add start command & modify server.js const port

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ app.use(express.json())
 app.use('/properties', propertyRoutes)
 app.use('/auth', authRoutes)
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
